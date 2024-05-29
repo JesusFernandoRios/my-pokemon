cd191f84e503a3820912b373f57ac35b5bfb4964
jesus-rios <jesus.rios@atventurepartners.tech>
2024-05-28 22:40:14 -0700

    working on pokemon card component layout

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,15 @@
 function PokemonCard() {
-  return <div>PokemonCard</div>;
+  return (
+    <div>
+        <p>Pokemon Summary</p>
+      <div>
+        <p>pokemon Name</p>
+        <p>Pokemon Image</p>
+        <p>Pokemon Abilities</p>
+        <p>Pokemon Moves</p>
+      </div>
+    </div>
+  );
 }
 
 export default PokemonCard;
